f7278122ded4f34e7268ab860ddf71f9638ea7bb
szylarq <piotrszylar6@wp.pl>
2019-04-12 07:45:27 +0200

    privilege
    privilege

---
+++ backend/src/main/java/com/example/springsocial/model/Privilege.java
@@ -0,0 +1,34 @@
+package com.example.springsocial.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="privilege")
+public class Privilege {
+
+    @Id
+    @Column(name = "IDAuthorization")
+    private int idAuthorization;
+    @Column(name = "Name")
+    private String name;
+
+    public Privilege(){
+    }
+
+    public Privilege (int idAuthorization, String name){
+
+        this.idAuthorization = idAuthorization;
+        this.name = name;
+    }
+
+    public int getIdAuthorization() {
+        return idAuthorization;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
