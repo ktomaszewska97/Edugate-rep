b89033f3670bc6978acfa619f96dfc688e82d733
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2019-05-22 13:21:37 +0200

    Zmiany pod ajaxa

--- src/main/java/edugate/demo/controllers/HomeController.java
+++ src/main/java/edugate/demo/controllers/HomeController.java
@@ -4,8 +4,13 @@ import edugate.demo.model.*;
 import edugate.demo.repositories.*;
 import edugate.demo.security.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
@@ -78,6 +83,33 @@ public class HomeController {
 
 
 		return mv;
+
+	}
+
+
+	@RequestMapping(value = {"/home/{school}","/home/{school}/{departament}","/home/{school}/{departament}/{field}","/home/{school}/{departament}/{field}/{course}"}, method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<Object> getHarm(@PathVariable("school") Integer school, @PathVariable("departament") Integer departament, @PathVariable("field") Integer field, @PathVariable("course") Integer course ) {
+		System.out.println("listExistingHarm is called in controller");
+
+		if (school == null) {
+			return new ResponseEntity<Object>(null, HttpStatus.BAD_REQUEST);
+				}
+		else{
+			if (departament == null) {
+				return new ResponseEntity<Object>(schoolRepository.findByIDSchool(school),HttpStatus.OK);
+			}
+			else
+				if(field == null){
+					return new ResponseEntity<Object>(departmentRepository.findByIDDepartament(departament),HttpStatus.OK);
+					}
+				else
+					if (course == null){
+						return  new ResponseEntity<Object>(fieldOfStudyRepository.findByIDFieldOfStudy(field),HttpStatus.OK);
+					}
+					else
+						return new ResponseEntity<Object>(courseRepository.findByIDCourse(course), HttpStatus.OK);
+		}
 
 	}
 
