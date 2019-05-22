50890b8ce1817c8a1fe3e82e1094498e36e6cb33
FractialCopper <35343054+FractialCopper@users.noreply.github.com>
2019-05-22 14:43:42 +0200

    update

--- src/main/java/edugate/demo/controllers/UserController.java
+++ src/main/java/edugate/demo/controllers/UserController.java
@@ -10,10 +10,16 @@ import edugate.demo.services.UserValidator;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpServerErrorException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -58,14 +64,23 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String login(Model model, String error, String logout) {
+    public String login(HttpServletRequest request,Model model, String error, String logout) {
 
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
 
-        if (logout != null)
+        if (logout != null) {
             model.addAttribute("message", "You have been logged out successfully.");
-
+            HttpSession session= request.getSession(false);
+            SecurityContextHolder.clearContext();
+            session= request.getSession(false);
+            if(session != null) {
+                session.invalidate();
+            }
+            for(Cookie cookie : request.getCookies()) {
+                cookie.setMaxAge(0);
+            }
+        }
         return "login";
     }
 /*
